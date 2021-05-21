50903ae186774ef60e95a0bd29ba9a008b9e965e
Saptarshi <saptarshichakraborty04@outlook.com>
2021-05-21 11:49:07 +0530

    Static Folder Removed Temporarily. Added 'Fetching Data form Database' in home page and post page. Design of Contact, Index and Post are Modified. -sc

--- main.py
+++ main.py
@@ -1,17 +1,29 @@
+import email
+from os import name
 from flask import Flask, render_template, request
 from flask.signals import message_flashed
 from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
+from datetime import date, datetime
+from flask_mail import Mail, Message
 import json
 
 
-with open('saptarshi.json','r') as c:
-    params = json.load(c) ["params"]
+with open('saptarshi.json', 'r') as c:
+    params = json.load(c)["params"]
+
 app = Flask(__name__)
+app.config.update(
+    MAIL_SERVER='smtp.gmail.com',
+    MAIL_PORT=456,
+    MAIL_USE_SSL='True',
+    MAIL_USERNAME=params['email-user'],
+    MAIL_PASSWORD=params['email-password']
+)
+mail = Mail(app)
+
 app.config['SQLALCHEMY_DATABASE_URI'] = params['database_uri']
 db = SQLAlchemy(app)
 # Note: If "No Module named MySQldb" error comes run "pip install mysqlclient" in Windows Powershell Admin
-
 
 class Contacts(db.Model):
     sno = db.Column(db.Integer, nullable=False, primary_key=True)
@@ -21,46 +33,67 @@ class Contacts(db.Model):
     msg = db.Column(db.String(150), nullable=False, primary_key=False)
     date = db.Column(db.String(17), nullable=True, primary_key=False)
 
+class Posts(db.Model):
+    sno = db.Column(db.Integer, nullable=False, primary_key=True)
+    title = db.Column(db.String(50), nullable=False, primary_key=False)
+    slug = db.Column(db.String(30), nullable=False, primary_key=False)
+    dn_link = db.Column(db.String(100), nullable=False, primary_key=False)
+    img_link = db.Column(db.String(30), nullable=False, primary_key=False)
+    details = db.Column(db.String(500), nullable=False, primary_key=False)
+    date = db.Column(db.String(17), nullable=True, primary_key=False)
 
-# Home Page
+
+# ~~~~ Home Page ~~~~
 @app.route("/")
-def home():
-    return render_template('index.html')
+def home_route():
+    posts=Posts.query.filter_by().all()
+    return render_template('index.html',params=params,posts=posts)
 
 
-# Contact Page
+# ~~~~ Contact Page ~~~~
 @app.route("/contact", methods=['GET', 'POST'])
-def contact():
+def contact_route():
     if(request.method == 'POST'):
         name = request.form.get('name')
         phone = request.form.get('phone')
         email = request.form.get('email')
         message = request.form.get('message')
-
-        entry = Contacts(name=name, email=email, phone_num=phone, msg=message,date=datetime.now())
+# Push to Database
+        entry = Contacts(name=name, email=email, phone_num=phone,
+                         msg=message, date=datetime.now())
         db.session.add(entry)
         db.session.commit()
 
-    return render_template('contact.html')
+        # mail.send_message('New Response in Website, form '+ name,
+        #           sender=name,
+        #           recipients=[params['email-user']],
+        #           body="New Responce form Contacts"+"\n" + "Name : " + name + "\n" + "Email : " + email + "\n" + "Message : " + message 
+        #           )
+    return render_template('contact.html',params=params)
 
 
-# Log In Page
+# ~~~~ Log In Page ~~~~~
 @app.route("/login")
-def login():
-    return render_template('login.html')
+def login_route():
+    return render_template('login.html',params=params)
 
 
-# Posts Page
-@app.route("/post")
-def posts():
-    return render_template('post.html')
+# ~~~~ Posts Page ~~~~
+@app.route("/post/<string:post_slug>", methods=['GET'])
+def posts_route(post_slug):
+    post=Posts.query.filter_by(slug=post_slug).first()
 
-# Dashboard Page
+
+
+
+    return render_template('post.html',params=params,post=post)
+
+# ~~~~ Dashboard Page ~~~~
 
 
 @app.route("/dashboard")
-def dashboard():
-    return render_template('dashboard.html')
+def dashboard_route():
+    return render_template('dashboard.html',params=params)
 
 
 app.run(debug=True)
